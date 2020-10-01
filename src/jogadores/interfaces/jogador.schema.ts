0d5e96610f45be140000ca9de9d708967c07d725
Daniel Obara <daniel.tsutomu@hotmail.com>
2020-09-30 22:26:52 -0300

    jogador schema

---
+++ src/jogadores/interfaces/jogador.schema.ts
@@ -0,0 +1,10 @@
+import * as mongoose from 'mongoose';
+
+export const JogadorSchema = new mongoose.Schema({
+	telefone: { type: String, unique: true },
+	email: { type: String, unique: true },
+	nome: { type: String },
+	ranking: { type: String },
+	posicaoRanking: { type: Number },
+	urlFotoJogador: { type: String },
+}, { timestamps: true, collection: 'jogadores' });
